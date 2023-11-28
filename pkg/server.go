ecf3e2c18932f710d1e92ee8b8d313e611da4e0a
희승 <heeseung@huiseung-ui-MacBookPro.local>
2023-11-28 23:47:49 +0900

    feat: add delete function to server interface

--- pkg/server.go
+++ pkg/server.go
@@ -2,6 +2,8 @@ package pkg
 
 import (
 	"errors"
+	"fmt"
+	"net/http"
 )
 
 type ServerService struct {
@@ -40,7 +42,38 @@ func (server *ServerService) List(url string) error {
 	return nil
 }
 
-func (server *ServerService) Delete(url string) error {
+func (server *ServerService) Delete(regionCode, serverInstanceNo, url string) error {
+	// apiURL 정의
+	url += fmt.Sprintf("?regionCode=%s&serverInstanceNoList.1=%d&responseFormatType=json",
+	 regionCode, serverInstanceNo,
+	)
+	
+	// httpRequest 생성
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return fmt.Errorf("Error creating request: ", err)
+	}
+	// httpRequest header 설정
+	GetCommonHeader(request)
+	
+	// httpRequest token 설정
+	SetAuthToken(request, server.token)
+
+	// HTTP 클라이언트 생성
+	client := &http.Client{}
+
+	// 요청 보내기
+	response, err := client.Do(request)
+	if err != nil {
+		return fmt.Errorf("Error sending request:", err)
+	}
+	defer response.Body.Close()
+	
+	// 결과 반환
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("Unexpected response status code:", err)
+	}
+	
 	return nil
 }
 
