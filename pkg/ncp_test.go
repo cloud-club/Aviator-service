b944d9cbfcb7fbca0630dcedff273ae0b4d01364
kim.doyoung <kim.doyoung@hyundai.com>
2023-12-04 22:04:39 +0900

    suite 테스트 완료

--- pkg/ncp_test.go
+++ pkg/ncp_test.go
@@ -1,48 +1,39 @@
 package pkg_test
 
 import (
-	"errors"
-	"fmt"
 	"github.com/cloud-club/Aviator-service/pkg"
 	"github.com/cloud-club/Aviator-service/types/auth"
 	"github.com/stretchr/testify/assert"
-	"log"
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
-func TestCreateAndVerifyToken_success(t *testing.T) {
-	ncp := pkg.NewNcpService("ncp service token")
-	ncp.Server = pkg.NewServerService("ncp server token")
-
-	err := ncp.CreateToken("admin", "CloudClubAdmin", []string{"admin"})
-	if err != nil {
-		t.Fatal(err)
-	}
-	isSuccess, err := ncp.VerifyToken(ncp.GetToken(), &auth.AuthTokenClaims{})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if isSuccess {
-		t.Log(ncp.GetToken())
-	} else {
-		t.Fatal(errors.New(fmt.Sprintf("token verify failed : %s", ncp.GetToken())))
-	}
+// This is our suite
+type NcpSuite struct {
+	suite.Suite
+	ncp *pkg.NcpService
 }
 
-func TestCreateAndVerifyToken_fail_token_is_invalid(t *testing.T) {
-	ncp := pkg.NewNcpService("ncp service token")
-	ncp.Server = pkg.NewServerService("ncp server token")
+// This gets run automatically by `go test` so we call `suite.Run` inside it
+func TestNcpSuite(t *testing.T) {
+	suite.Run(t, new(NcpSuite))
+}
 
-	err := ncp.CreateToken("admin", "CloudClubAdmin", []string{"admin"})
-	if err != nil {
-		t.Fatal(err)
-	}
-	isSuccess, err := ncp.VerifyToken(ncp.GetToken()+"is failed", &auth.AuthTokenClaims{})
-	log.Println(err)
-	if isSuccess {
-		t.Fail()
-	} else {
-		assert.Error(t, err, errors.New("token signature is invalid"))
-	}
+// This method gets run only first time before starting test
+func (suite *NcpSuite) SetupSuite() {
+	suite.ncp = pkg.NewNcpService("ncp service token")
+	suite.ncp.Server = pkg.NewServerService("ncp server token")
+
+	err := suite.ncp.CreateToken("admin", "CloudClubAdmin", []string{"admin"})
+	assert.NoError(suite.T(), err, nil)
+}
+
+func (suite *NcpSuite) TestVerifyToken_success() {
+	_, err := suite.ncp.VerifyToken(suite.ncp.GetToken(), &auth.AuthTokenClaims{})
+	suite.Equal(err, nil, nil)
+}
+
+func (suite *NcpSuite) TestCreateAndVerifyToken_fail_token_is_invalid() {
+	_, err := suite.ncp.VerifyToken(suite.ncp.GetToken()+"is failed", &auth.AuthTokenClaims{})
+	suite.EqualError(err, "token signature is invalid")
 }
